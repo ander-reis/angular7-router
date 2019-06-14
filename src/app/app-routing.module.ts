21c30d899a37290f1956dce9c6fa192bcc12852e
Desktop Casa <ander-reis@hotmail.com>
2019-06-14 12:44:35 -0300

    Criando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {PrincipalComponent} from './principal/principal.component';
+import {SobreComponent} from './sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: PrincipalComponent},
+    {path: 'sobre', component: SobreComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
